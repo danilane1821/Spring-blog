4e04b9593c27da3e9b8ea97f0caca29ae8730e34
danielle lane <daniellelane1821@gmail.com>
2019-11-05 14:44:23 -0600

    fixed and finished first one"

--- src/main/java/com/codeup/blog/blog/controllers/RollDiceController.java
+++ src/main/java/com/codeup/blog/blog/controllers/RollDiceController.java
@@ -14,19 +14,15 @@ public class RollDiceController {
         return "roll-dice";
     }
 
+
     @GetMapping("/roll-dice/{n}")
-    public String guessNumber(@PathVariable (name = "n") int n, Model viewModel) {
+    public String guessNumber(@PathVariable (name = "n") int n,Model viewModel) {
         Random rand = new Random();
         int rand_int = rand.nextInt(6)+1;
-        if(n == rand_int){
-            viewModel.addAttribute("correct", "You guessed correct! The dice rolled " + rand_int + " and you guessed " + n);
-        }else {
-            viewModel.addAttribute("wrong", "Sorry! You guessed wrong.. The dice rolled " + rand_int + " and you guessed " + n);
-        }
-        return "roll-dice";
+            viewModel.addAttribute("answer",n == rand_int);
+            viewModel.addAttribute("random",rand_int);
+            return "roll-dice";
     }
-
-
 
 
 }
