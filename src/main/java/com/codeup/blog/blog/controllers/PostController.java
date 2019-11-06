15b3c27a8ec50e9225064b52809f1338aaf07fa1
danielle lane <daniellelane1821@gmail.com>
2019-11-06 10:53:27 -0600

    tiny exercise about dogs done

--- src/main/java/com/codeup/blog/blog/controllers/PostController.java
+++ src/main/java/com/codeup/blog/blog/controllers/PostController.java
@@ -10,38 +10,38 @@ import java.util.ArrayList;
 @Controller
 public class PostController {
 
-    @GetMapping("/")
-    public String index(Model viewModel) {
-        ArrayList<Posts> postList = new ArrayList<Posts>();
-        postList.add(new Posts(2,"second Post","skjdsdkh"));
-        postList.add(new Posts(3,"third Post","jndjsnu"));
-
-        viewModel.addAttribute("posts", postList);
-        return "index";
-    }
-
-
-
-    @GetMapping("/show/{id}")
-
-    public String showById(@PathVariable (name = "id") long id, Model viewModel){
-        Posts post = new Posts(1,"new","fjdkfjk");
-        viewModel.addAttribute("post",post);
-
-        return "show";
-    }
-
-    @GetMapping("/posts/create")
-    @ResponseBody
-    public String showForm(){
-        return "view the form for creating a post";
-    }
-
-    @PostMapping("/posts/create")
-    @ResponseBody
-    public String create(@RequestParam String title,@RequestParam String body){
-        System.out.println("title = " + title);
-        System.out.println("body = " + body);
-        return "create a new post";
-    }
+//    @GetMapping("/")
+//    public String index(Model viewModel) {
+//        ArrayList<Posts> postList = new ArrayList<Posts>();
+//        postList.add(new Posts(2,"second Post","skjdsdkh"));
+//        postList.add(new Posts(3,"third Post","jndjsnu"));
+//
+//        viewModel.addAttribute("posts", postList);
+//        return "index";
+//    }
+//
+//
+//
+//    @GetMapping("/show/{id}")
+//
+//    public String showById(@PathVariable (name = "id") long id, Model viewModel){
+//        Posts post = new Posts(1,"new","fjdkfjk");
+//        viewModel.addAttribute("post",post);
+//
+//        return "show";
+//    }
+//
+//    @GetMapping("/posts/create")
+//    @ResponseBody
+//    public String showForm(){
+//        return "view the form for creating a post";
+//    }
+//
+//    @PostMapping("/posts/create")
+//    @ResponseBody
+//    public String create(@RequestParam String title,@RequestParam String body){
+//        System.out.println("title = " + title);
+//        System.out.println("body = " + body);
+//        return "show";
+//    }
 }
