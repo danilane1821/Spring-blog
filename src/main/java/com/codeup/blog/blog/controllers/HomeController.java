58d6de343863ee271ce480a98a8431cac9286213
danielle lane <daniellelane1821@gmail.com>
2019-11-13 17:02:25 -0600

    broke

--- src/main/java/com/codeup/blog/blog/controllers/HomeController.java
+++ src/main/java/com/codeup/blog/blog/controllers/HomeController.java
@@ -1,6 +1,9 @@
 package com.codeup.blog.blog.controllers;
 
 //import org.springframework.security.core.context.SecurityContextHolder;
+import com.codeup.blog.blog.Models.User;
+import com.codeup.blog.blog.repositories.PostsRepository;
+import com.codeup.blog.blog.repositories.UserRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,9 +11,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+//    private final PostsRepository postDao;
+//    public final UserRepository userDao;
+//
+//    public HomeController(PostsRepository postDao, UserRepository userDao) {
+//        this.postDao = postDao;
+//        this.userDao = userDao;
+//    }
+
     @GetMapping("/")
     public String index(Model viewModel){
-        System.out.println((SecurityContextHolder.getContext().getAuthentication().getPrincipal()));
+        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return "posts/index";
     }
+
+
 }
--- src/main/java/com/codeup/blog/blog/repositories/PostsRepository.java
+++ src/main/java/com/codeup/blog/blog/repositories/PostsRepository.java
@@ -1,9 +1,14 @@
 package com.codeup.blog.blog.repositories;
 
 import com.codeup.blog.blog.Models.Post;
+import com.codeup.blog.blog.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PostsRepository extends JpaRepository<Post, Long> {
+
 }
